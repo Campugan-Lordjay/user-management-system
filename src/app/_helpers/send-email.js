cc3d3911a5d19d23395345b36f9e98cbfc65cd28
Asyze <loydjayvillafuerte@gmail.com>
2025-04-12 11:56:42 +0800

    [Initialize commit] add :swagger.js

--- src/app/_helpers/send-email.js
+++ src/app/_helpers/send-email.js
@@ -7,3 +7,4 @@ async function sendEmail({to, subject, html, from = config.emailForm}) {
     const transporter = nodemailer.createTransport(config.smtpOptions);
     await transporter.sendMail({from, to, subject, html});
 }
+
